5754aa6b6421c8f98a061c989a0907e7fc0d285b
palsandy789 <57903328+palsandy789@users.noreply.github.com>
2019-11-18 19:27:07 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,128 @@
+<?php include("include/top-header.php"); ?>
+
+		<!-- header-start -->
+<?php include("include/header.php"); ?>
+		<!-- header-end -->
+
+		<!-- slider-start -->
+<?php include("include/slider.php"); ?>		
+		<!-- slider-end -->
+
+		<!-- about-us-area-start -->
+		<div class="about-us-area about-shape pt-120 pb-90">
+			<div class="container">
+				<div class="row">
+					<div class="col-xl-6 col-lg-6">
+						<div class="about-text mb-30">
+						<!--	<span>about us</span> -->
+							<h2 style="color:#28a745;">Mushroom Cultivation.</h2>
+							<p>Organature Agro Foods is committed to growing the fastest and the finest quality of button mushrooms in Uttrakhand region. We use sustainable practices which are in the best interest of both environment and consumers. We have invested the most superior resources in refining our growing techniques. Our produce appeals to a wide audience base. In a short span, we have gained our reputation as the grower of top quality, full of flavour button mushrooms. </p>
+							<a class="btn" href="javascript:void(0);">Read More</a>
+						</div>
+					</div>
+					<div class="col-xl-6 col-lg-6">
+						<div class="about-img mb-30">
+							<img  src="img/about/2.jpg" alt="" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- about-us-area-end -->
+
+		<!-- Mission-us-area-start -->
+		<div class="about-us-area about-shape">
+			<div class="container">
+				<div class="row" style="border: solid 0px red;">
+					
+					<div class="col-xl-6 col-lg-6">
+							<img  src="img/about/2.jpg" alt="" />
+					</div>
+
+					<div class="col-xl-6 col-lg-6">
+						<div class="about-text mb-30">
+						<!--	<span>about us</span> -->
+							<h1>Mission</h1>
+							<p>We are proud suppliers to wholesalers who swear by our products. We intend to reach out to more mushrooms eaters in times to come. Our aim has been to put focus on innovation and provide quality to consumers so that they get the healthiest and best tasting mushrooms.</p>
+							<p>With wide experience of the workers on farm, we have the knowledge, experience and finesse required to continue improving our processes so that Organature Agro Foods can keep growing better mushrooms. </p>
+							<a class="btn" href="javascript:void(0);">Read More</a>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12">&nbsp;</div>
+			</div>
+		</div>
+		<!-- Mission-us-area-end -->
+
+		<!-- organic-area-start -->
+		<div class="organic-area pt-165 pb-180" style="background-image:url(img/bg/bg11.jpg)">
+			<div class="container">
+				<div class="row">
+					<div class="col-xl-6 offset-xl-6 col-lg-6 offset-lg-6 col-md-10 offset-md-2">
+						<div class="organic-wrapper">
+							<div class="organic-text">
+								<h1>What are Button Mushrooms ?</h1>
+								<p>The Button Mushroom ( Agaricus Bisporous) is a grilled fungus. It is also more commonly known as Table Mushroom; White mushroom; Common Mushroom and Cultivated Mushroom. They are known to be rich in vitamins and minerals. They specially contain high amount of Vitamin B and Potassium. They also have very low energy levels.  For example, five button sized mushrooms put together have only 20 calories. </p>
+								<a class="btn" href="javascript:void(0);">Read More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- organic-area-end -->
+
+
+
+		<!-- counter-area-start -->
+		<div class="counter-area pt-120 pb-85" style="background-image:url(img/bg/bg12.jpg)">
+			<div class="container">
+				<div class="counter-bg mb-60">
+					<div class="row">
+						<div class="col-xl-4 col-lg-4 col-md-4">
+							<div class="counter-section mb-30">
+								<h1>30</h1>
+							</div>
+						</div>
+						<div class="col-xl-8 col-lg-8 col-md-8">
+							<div class="counter-info mb-30">
+								<h1>Years of Industry <br> Experience</h1>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua enim ad minim veniam.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		<!-- counter-area-end -->
+
+		<!-- best-deals-area-start -->
+		<div class="best-deals-area pt-120 pb-90">
+			<div class="container">
+				<div class="row">
+					<div class="col-xl-7 col-lg-7">
+						<div class="best-deals-img mb-30">
+							<img src="img/bg/3.png" alt="" />
+						</div>
+					</div>
+					<div class="col-xl-5 col-lg-5">
+						<div class="best-deals-wrapper text-center mb-30">
+							<div class="best-deals-content">
+								<h1>Recipes</h1>
+								<p class="text-left">Mushrooms have inspired exotic recipes ranging from breakfast to dinner. There are plethora of recipes you could prepare using button mushrooms from Organature Agro Foods. White Mushrooms more popularly known as button mushrooms have a fairly mild taste and blend with almost everything. Their flavour intensifies when cooked. Take a look and get inspired to buy and prepare our mushrooms.</p>
+								<a class="btn" href="javascript:void(0);">Read More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- best-deals-area-end -->
+		<!-- footer-area-start -->
+<?php include("include/footer.php"); ?>		
+		<!-- footer-area-end -->  
+<?php include("include/footer-js.php"); ?>
